4d18eb15ac2bda11b6e73a098c15fdbb72b64a54
hebert2017 <49122058+hebert2017@users.noreply.github.com>
2019-12-11 20:27:38 -0500

    Add files via upload
    codigo queue

--- main.cpp
+++ main.cpp
@@ -1,22 +1,25 @@
+// CPP program to illustrate
+// Implementation of push() function
 #include <iostream>
-#include <stack>
+#include <queue>
 using namespace std;
 
 int main()
 {
-    // Empty stack
-    stack<int> mystack;
-    mystack.push(0);
-    mystack.push(1);
-    mystack.push(2);
-    mystack.push(3);
+    // Empty Queue
+    queue<int> myqueue;
+    myqueue.push(0);
+    myqueue.push(1);
+    myqueue.push(2);
+    myqueue.push(3);
 
-    // Printing content of stack
-    while (!mystack.empty()) {
-        cout << ' ' << mystack.top()<<endl;
-        cout<<mystack.top()<< endl;
-        cout<<mystack.size()<< endl;
-        mystack.pop();
+    // Printing content of queue
+    while (!myqueue.empty()) {
+        cout << ' ' << myqueue.front()<<endl;
+
+        cout<<"FRENTE"<<myqueue.front()<< endl;
+        cout<<<<myqueue.back()<< endl;
+        myqueue.pop();
     }
+    return 0;
 }
-
